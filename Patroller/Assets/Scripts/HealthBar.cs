13a8fe00219569592f9c6eb6dab1a624d668d5ed
Eric02 <1486406027@qq.com>
2019-11-17 17:58:28 +0800

    Finish HW9
    health bar

---
+++ Patroller/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthBar : MonoBehaviour
+{
+    private FirstController SceneController;
+    public Slider mySlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneController = SSDirector.GetInstance().CurrentScenceController as FirstController;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int life = SceneController.GetLife();
+        if (life == 1)
+        {
+            mySlider.value = 100;
+        }
+        else
+        {
+            mySlider.value = 0;
+        }
+        this.transform.LookAt(this.transform.position - new Vector3(0f, -2f, 1f));
+    }
+}
--- Patroller/Assets/Scripts/UserGUI.cs
+++ Patroller/Assets/Scripts/UserGUI.cs
@@ -17,6 +17,7 @@ public class UserGUI : MonoBehaviour
     {
         GUISkin skin = GUI.skin;
         skin.button.normal.textColor = Color.black;
+        skin.horizontalScrollbar.normal.textColor = Color.red;
         skin.label.normal.textColor = Color.black;
         skin.button.fontSize = 20;
         skin.label.fontSize = 20;
@@ -25,10 +26,12 @@ public class UserGUI : MonoBehaviour
 
         if(life > 0)
         {
+            //GUI.HorizontalScrollbar(new Rect(0, 5, Screen.width / 8, Screen.height / 32), 1.0f, 1.0f, 0.0f, 1.0f);
             GUI.Label(new Rect(0, Screen.height / 16, Screen.width / 8, Screen.height / 16), "Score:" + action.GetScore().ToString());
         }
         else
         {
+            //GUI.HorizontalScrollbar(new Rect(0, 5, Screen.width / 8, Screen.height / 32), 0.0f, 1.0f, 0.0f, 1.0f);
             GUI.Label(new Rect(Screen.width/2-60, Screen.height*5/16, 120, Screen.height / 8), "Game Over!");
             GUI.Label(new Rect(Screen.width/2-75, Screen.height*7/16, 200, Screen.height / 8), "Your score is:"+ action.GetScore().ToString());
             if (GUI.Button(new Rect(Screen.width * 3 / 8, Screen.height * 9 / 16, Screen.width / 4, Screen.height / 8), "Restart"))
